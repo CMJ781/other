224b363887b6490d0404dd8dba76393fdb8bcbb2
siropboy <64370904+siropboy@users.noreply.github.com>
2020-11-30 10:32:54 +0800

    Update update_package_mini.sh

--- patch/script/update_package_mini.sh
+++ patch/script/update_package_mini.sh
@@ -21,12 +21,7 @@ sed -i 's/$(VERSION_DIST_SANITIZED)/$(shell TZ=UTC-8 date +%Y%m%d)-Ipv6-mini/g' 
 sed -i 's/invalid/# invalid/g' package/network/services/samba36/files/smb.conf.template
 echo "DISTRIB_REVISION='S$(TZ=UTC-8 date +%Y.%m.%d) Sirpdboy mini'" > ./package/base-files/files/etc/openwrt_release1
 sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' ./package/base-files/files/etc/shadow
-git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
-# svn co https://github.com/jerrykuku/luci-app-jd-dailybonus/trunk/ ./package/diy/luci-app-jd-dailybonus
-# git clone -b master --single-branch https://github.com/tty228/luci-app-serverchan ./package/diy/luci-app-serverchan
-# git clone -b master --single-branch https://github.com/destan19/OpenAppFilter ./package/diy/OpenAppFilter
-# svn co https://github.com/siropboy/luci-app-vssr-plus/trunk/luci-app-vssr-plus package/new/luci-app-vssr-plus
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ package/diy/lienol
-git clone -b master https://github.com/xiaorouji/openwrt-passwall.git package/lienol
+git clone -b master https://github.com/vernesong/OpenClash package/OpenClash
+git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 svn co https://github.com/jerrykuku/luci-app-vssr/trunk/  package/diy/luci-app-vssr
 ./scripts/feeds update -i
