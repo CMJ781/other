4f2258d51c9083af9c57e00b10a64d38215c85ba
siropboy <64370904+siropboy@users.noreply.github.com>
2021-01-04 18:02:44 +0800

    transmission

--- patch/script/update_package_all.sh
+++ patch/script/update_package_all.sh
@@ -8,6 +8,9 @@ rm -rf ./package/lean/v2ray-plugin
 rm -rf ./package/lean/xray
 rm -rf ./package/lean/luci-theme-opentomcat
 rm -rf ./feeds/packages/net/smartdns
+rm -rf ./feeds/packages/net/transmission-web-control
+rm -rf ./feeds/packages/net/transmission
+rm -rf ./feeds/luci/applications/luci-app-transmission
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/smartdns ./package/diy/smartdns
 rm -rf ./package/lean/luci-app-netdata
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata ./package/lean/luci-app-netdata
