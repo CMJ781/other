b247ceefaf14edfb19995c4302cc13c73612c68d
siropboy <64370904+siropboy@users.noreply.github.com>
2021-01-06 09:52:56 +0800

    Update uppackage_mini.sh

--- patch/script/uppackage_mini.sh
+++ patch/script/uppackage_mini.sh
@@ -79,6 +79,5 @@ rm -rf ./package/diy1/xray
 #  make && sudo make install
 rm -rf package/diy/luci-app-dockerman
 rm -rf package/diy/luci-lib-docker
-rm -rf package/diy/parted
 
 ./scripts/feeds update -i
