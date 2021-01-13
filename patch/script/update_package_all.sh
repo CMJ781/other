43c5998492a6269add5689ff1e01d31ec9c456fc
siropboy <64370904+siropboy@users.noreply.github.com>
2021-01-13 22:13:44 +0800

    https-dns-proxy

--- patch/script/update_package_all.sh
+++ patch/script/update_package_all.sh
@@ -36,8 +36,7 @@ rm -rf ./feeds/packages/admin/netdata && \
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/netdata ./feeds/packages/admin/netdata
 rm -rf ./feeds/packages/net/mwan3 && \
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/mwan3 ./feeds/packages/net/mwan3
-rm -rf ./feeds/packages/net/https-dns-proxy && \
-svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy ./feeds/packages/net/https-dns-proxy
+rm -rf ./feeds/packages/net/https-dns-proxy 
 
 #rm -rf ./package/diy/autocore
 #rm -rf ./package/diy/default-settings
