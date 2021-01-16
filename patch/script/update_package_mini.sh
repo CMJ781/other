3f9a8704ff09e997333d8e186fc27d8757321867
siropboy <64370904+siropboy@users.noreply.github.com>
2021-01-16 11:08:11 +0800

    d

--- patch/script/update_package_mini.sh
+++ patch/script/update_package_mini.sh
@@ -38,7 +38,7 @@ cp -f ./package/diy/banner ./package/base-files/files/etc/
 date1='Ipv6-Mini-S'`TZ=UTC-8 date +%Y.%m.%d -d +"0"days`
 sed -i 's/$(VERSION_DIST_SANITIZED)/$(VERSION_DIST_SANITIZED)-${date1}/g' include/image.mk
 echo "DISTRIB_REVISION='${date1} by Sirpdboy'" > ./package/base-files/files/etc/openwrt_release1
-echo ' %D '${date1}' by Sirpdboy ' >> ./package/base-files/files/etc/banner
+echo ${date1}' by Sirpdboy ' >> ./package/base-files/files/etc/banner
 echo ' --------------------------------' >> ./package/base-files/files/etc/banner
 sed -i 's/带宽监控/监控/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
 sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' ./package/base-files/files/etc/shadow
