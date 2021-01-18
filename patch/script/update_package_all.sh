1dd629c00025f898ae29aa0b1612bc9e53534193
siropboy <64370904+siropboy@users.noreply.github.com>
2021-01-19 02:47:29 +0800

    setbypasss

--- patch/script/update_package_all.sh
+++ patch/script/update_package_all.sh
@@ -75,11 +75,11 @@ echo  'CONFIG_NVME_FABRICS=y'  >> ./package/target/linux/x86/config-5.4
 echo  'CONFIG_NVME_FC=y' >> ./package/target/linux/x86/config-5.4
 echo  'CONFIG_NVME_MULTIPATH=y' >> ./package/target/linux/x86/config-5.4
 echo  'CONFIG_NVME_TCP=y' >> ./package/target/linux/x86/config-5.4
-#git clone https://github.com/garypang13/luci-app-bypass.git package/diy/luci-app-bypass
-#git clone https://github.com/garypang13/luci-app-dnsfilter.git package/diy/luci-app-dnsfilter
+git clone https://github.com/garypang13/luci-app-bypass.git package/diy/luci-app-bypass
+git clone https://github.com/garypang13/luci-app-dnsfilter.git package/diy/luci-app-dnsfilter
 git clone https://github.com/BCYDTZ/luci-app-UUGameAcc.git package/diy/luci-app-UUGameAcc
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/diy/luci-app-jd-dailybonus
-svn co https://github.com/jerrykuku/luci-app-vssr/trunk/  package/diy/luci-app-vssr
+#svn co https://github.com/jerrykuku/luci-app-vssr/trunk/  package/diy/luci-app-vssr
 #svn co https://github.com/jerrykuku/luci-app-jd-dailybonus/trunk/ ./package/diy/luci-app-jd-dailybonus
 #git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/diy/luci-app-openclash
@@ -108,5 +108,5 @@ git clone -b master --single-branch https://github.com/destan19/OpenAppFilter ./
 #  rm -rf package/lean/parted
 rm -rf package/diy/luci-app-dockerman
 rm -rf package/diy/luci-lib-docker
-
+rm -rf package/diy/vssr
 ./scripts/feeds update -i
