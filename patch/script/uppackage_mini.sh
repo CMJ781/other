03a29071ef4a17b50acea22c1b909d3782909212
loso3000 <51484662+loso3000@users.noreply.github.com>
2021-02-04 10:19:56 +0800

    smab4

--- patch/script/uppackage_mini.sh
+++ patch/script/uppackage_mini.sh
@@ -26,7 +26,9 @@ rm -rf ./package/lean/luci-app-cpufreq
 rm -rf ./package/lean/luci-app-ipsec-vpnd
 rm -rf ./package/lean/luci-app-wrtbwmon
 rm -rf ./package/lean/samba4
-rm -rf ./package/lean/shadowsocksr-libev
+rm -rf package/lean/luci-app-samba4
+#rm -rf ./package/diy/luci-app-samba4
+rm -rf ./package/diy/shadowsocksr-libev
 #rm -rf ./package/diy/autocore
 rm -rf ./package/diy/netdata
 rm -rf ./package/diy/mwan3
@@ -51,8 +53,6 @@ echo ' --------------------------------' >> ./package/base-files/files/etc/banne
 sed -i 's/$(VERSION_DIST_SANITIZED)/$(shell TZ=UTC-8 date +%Y%m%d)-Ipv4P-Mini/g' include/image.mk
 #sed -i 's/by/Ipv4P Mini S$(TZ=UTC-8 date +%Y.%m.%d) by/g'  ./package/base-files/files/etc/banner
 #echo "DISTRIB_REVISION='Ipv4P Mini S$(TZ=UTC-8 date +%Y.%m.%d) by Sirpdboy'" > ./package/base-files/files/etc/openwrt_release1 
-rm -rf package/lean/luci-app-samba4
-#rm -rf ./package/diy/luci-app-samba4
 sed -i 's/invalid/# invalid/g' package/lean/samba4/files/smb.conf.template
 sed -i 's/invalid/# invalid/g' package/network/services/samba36/files/smb.conf.template
 sed -i 's/带宽监控/监控/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
